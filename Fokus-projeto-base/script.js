fd17f1460d83439de3df322cf1455c2438192a8f
Patrick Cavalcante <patrickcorreacavalcante@gmail.com>
2023-10-04 10:34:54 -0300

    Alteração de caminho e códigos.

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,26 +4,49 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    }
+    else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
     /*html.setAttribute('data-contexto', 'foco')
     banner.setAttribute('src', './imagens/foco.png')*/
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     /*html.setAttribute('data-contexto', 'descanso-curto')
     banner.setAttribute('src', './imagens/descanso-curto.png')*/
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     /*html.setAttribute('data-contexto', 'descanso-longo')
     banner.setAttribute('src', './imagens/descanso-longo.png')*/
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
 
@@ -46,5 +69,17 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    iniciar()
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', contagemRegressiva)
+
+function iniciar () {
+    intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
